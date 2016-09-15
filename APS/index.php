3d027b2e4ab6730a0be8cb5c684be268cc21a9e0
Malen <s3539863@student.rmit.edu.au>
2016-09-15 14:40:22 +1000

    Deleted Folder

---
+++ APS/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Destination</title>
+</head>
+<body>
+<h1>Please choose from the following options</h1>
+<a href="/aps/loginbank/bankindex.php">Public Bank</a> <br> 
+<a href="/aps/loginwebsite/shopindex.php">Shop</a>  
+</body>
+</html>
